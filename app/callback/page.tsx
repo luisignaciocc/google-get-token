273cd0be45dce7afd563254eccf1081ddf7a0a45
Luis Ignacio <luisignacioccp@gmail.com>
2024-10-16 20:53:23 -0300

    Wrap TokenExchangeComponent in Suspense for improved loading handling

--- app/callback/page.tsx
+++ app/callback/page.tsx
@@ -1,9 +1,12 @@
 import { TokenExchangeComponent } from "@/components/token-exchange";
+import { Suspense } from "react";
 
 export default function Home() {
   return (
     <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-      <TokenExchangeComponent />
+      <Suspense>
+        <TokenExchangeComponent />
+      </Suspense>
     </main>
   );
 }
